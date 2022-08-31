de25bf34ce1549aa6d9b0dc7404ad788cfbf6ab4
Camycog <cami.garcia@duocuc.cl>
2022-08-31 14:40:49 -0400

    registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
     path: 'productos',
     loadChildren: () => import('./productos/productos.module').then( m => m.ProductosPageModule)
   },
+  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+  },
 
 ];
 
