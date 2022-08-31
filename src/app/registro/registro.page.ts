b44b541e3f2eec9253b3338469c4f129d7f9bde2
Camycog <cami.garcia@duocuc.cl>
2022-08-31 15:10:16 -0400

    registro nuevo usuario

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
-
-  constructor() { }
-
+  data: any;
   ngOnInit() {
   }
-
+  constructor(private activeroute: ActivatedRoute, private router: Router) {
+    // Se llama a la ruta activa y se obtiene sus parametros mediante una subscripcion
+    this.activeroute.queryParams.subscribe(params => { // Utilizamos lambda
+      if (this.router.getCurrentNavigation().extras.state) { // Validamos que en la navegacion actual tenga extras
+        this.data = this.router.getCurrentNavigation().extras.state.user; // Si tiene extra rescata lo enviado
+        console.log(this.data) // Muestra por consola lo traido
+      }else{this.router.navigate(["/login"])} // Si no tiene extra la navegacion actual navegar al login
+    });
+  }
 }
