d92e1aee47f41bf413180580918e8a6c4ccaf765
Camycog <cami.garcia@duocuc.cl>
2022-08-31 16:06:03 -0400

    :D

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -11,34 +12,22 @@ export class LoginPage implements OnInit {
    * Se genera el modelo user con dos claves
    * cada clave tiene su valor inicial
    */
-  user = {
-    usuario: "",
-    password: ""
-  }
-  newuser = {
-    usuario: "",
-    password: ""
+  user={
+    usuario:"",
+    password:""
   }
   constructor(private router: Router) { } // Se debe instanciar
+
   ngOnInit() {
   }
-  ingresar() {
+  ingresar(){
     // Se declara e instancia un elemento de tipo NavigationExtras
     let navigationExtras: NavigationExtras = {
       state: {
         user: this.user // Al estado se asignamos un objeto con clave y valor
       }
-
-    }
-  };
-  registro() {
-    // Se declara e instancia un elemento de tipo NavigationExtras
-    let navigationExtras: NavigationExtras = {
-      state: {
-        newuser: this.newuser // Al estado se asignamos un objeto con clave y valor
-      }
     };
-    this.router.navigate(['/registro'], navigationExtras); // navegamos hacia el Home y enviamos información adicional
+    this.router.navigate(['/home'],navigationExtras); // navegamos hacia el Home y enviamos información adicional
   }
-};
 
+}
